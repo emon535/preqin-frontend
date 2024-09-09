ca43d9d8c4688c412b82fdcef26c7e2fc8c28380
emon535 <emon535@gmail.com>
2024-09-09 02:12:33 +0100

    test setup

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,12 +1,17 @@
 // utils/formatNumber.ts
-export const formatNumber = (num: number|string): string => {
+export const formatNumber = (num: number | string): string => {
     if (typeof num === 'string') {
       num = parseFloat(num);
     }
+  
+    if (isNaN(num)) {
+      return 'NaN'; // Handle non-numeric strings
+    }
+  
     if (num >= 1_000_000_000) {
-      return `${(num / 1_000_000_000).toFixed(1)}B`;
+      return `${(num / 1_000_000_000).toFixed(1).replace(/\.0$/, '')}B`;
     } else if (num >= 1_000_000) {
-      return `${(num / 1_000_000).toFixed(1)}M`;
+      return `${(num / 1_000_000).toFixed(1).replace(/\.0$/, '')}M`;
     } else {
       return num.toLocaleString();
     }
